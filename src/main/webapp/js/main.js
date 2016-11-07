2c42368a4e7c1a03fa270146a5c755e1b2db8f07
Sheng <sheng.di@citrix.com>
2016-11-07 11:10:53 -0800

    add rest api call

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,18 +1,25 @@
 $(document).ready(function () {
-    $('.btn-ok').click(function(){
-        var param1 = $('.configuration1').val();
-        var param2 = $('.configuration2').val();
-        var param3 = $('.configuration3').val();
-        
-        var url = "www.example.com" + "?configuration1=" + param1
-            + "configuration2=" + param2
-            + "configuration3=" + param3;
+    $('.btn-ok').click(function () {
+        $.ajax({
+            type: "POST",
+            url: 'www.example.com', // post url
+            data: {}, // the data passed to post
+            success: function (data) {
+                var param1 = $('.configuration1').val();
+                var param2 = $('.configuration2').val();
+                var param3 = $('.configuration3').val();
 
-        if(!$('#iframe').hasClass('active')) {
-            $('#iframe').addClass('active');
-        }
-        $('#iframe').attr('src', url);
+                var url = "www.example.com" + "?configuration1=" + param1
+                    + "configuration2=" + param2
+                    + "configuration3=" + param3;
 
-        $('#configuration-modal').modal('hide');
+                if (!$('#iframe').hasClass('active')) {
+                    $('#iframe').addClass('active');
+                }
+                $('#iframe').attr('src', url);
+
+                $('#configuration-modal').modal('hide');
+            }
+        });
     });
 });
