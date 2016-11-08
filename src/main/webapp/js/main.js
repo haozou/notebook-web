381fa7fe00acdd1017e004798a8281add9a96615
haozou <zou.ha@husky.neu.edu>
2016-11-08 15:00:29 -0800

    enhancement

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -3,22 +3,37 @@ $(document).ready(function () {
         var username = $('.username').val();
         var image_name = $('.image_name').val();
         var command = $('.command').val();
-        $.ajax({
+
+        var data = {"username": username,
+         "image_name": image_name,
+         "command": command}
+
+         var ret = post('http://localhost:8000/api/v1.0/run_notebook_in_docker', data);
+         ret.done(function(result, status) {
+            if (status == "success") {
+                name = result.container_name;
+                port = result.port;
+                ret = post("http://localhost:8000/api/v1.0/creates", {"target_dir": "notebook_data/" + name, "filename": "test.ipynb"});
+                ret.done(function(result, status) {
+                    if (status == "success") {
+                        if (!$('#iframe').hasClass('active')) {
+                             $('#iframe').addClass('active');
+                         }
+                         $('#iframe').attr('src', "http://localhost:" + port + "/notebooks/test.ipynb");
+                    }
+                });
+            }
+         });
+         $('#configuration-modal').modal('hide');
+    });
+
+    function post(uri, data) {
+        var ret = $.ajax({
             type: "POST",
             dataType: "json",
-            url: 'http://mbr-31574:8000/api/v1.0/run_notebook_in_docker', // post url
-            data: {"username": username,
-                   "image_name": image_name,
-                   "command": command}, // the data passed to post
-            success: function (result) {
-
-                if (!$('#iframe').hasClass('active')) {
-                    $('#iframe').addClass('active');
-                }
-                $('#iframe').attr('src', "http://localhost:" + result.port + "/notebooks/test2.ipynb");
-
-                $('#configuration-modal').modal('hide');
-            }
+            url: uri, // post url
+            data: data // the data passed to post
         });
-    });
+        return ret;
+    }
 });
